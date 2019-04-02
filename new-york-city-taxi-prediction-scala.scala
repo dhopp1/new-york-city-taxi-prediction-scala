28f63f887c8c5a5cace6713d20967348a884a36d
Daniel Hopp <daniel.hopp1@gmail.com>
2019-04-02 18:44:40 +0200

    Parsing datetime, start dow function

--- new-york-city-taxi-prediction-scala.scala
+++ new-york-city-taxi-prediction-scala.scala
@@ -9,10 +9,28 @@ def create_features(df: org.apache.spark.sql.DataFrame, train_set: Boolean): org
    //converting strings to double
   var columns = Array("fare_amount", "pickup_longitude", "pickup_latitude", "dropoff_longitude", "dropoff_latitude", "passenger_count") 
   var temp = df
+  //robust to a train or test set, without the fare amount column
   for(column <- columns){
     if(train_set || column != "fare_amount"){
        temp = temp.withColumn(column, temp(column).cast(org.apache.spark.sql.types.DoubleType))
     }
   }
+  //changing datetimes from strings to datetimes
+  temp = temp.withColumn("pickup_datetime", org.apache.spark.sql.functions.to_timestamp(temp("pickup_datetime"), "yyyy-MM-dd HH:mm:ss"))
   return temp
 }
+
+// COMMAND ----------
+
+//making function to get day of week from datetime
+def get_dow(input: String): java.time.DayOfWeek = {
+    java.time.LocalDate.parse(input,java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")).getDayOfWeek
+}
+//to apply to everything in a column
+val round_tenths_place_udf = udf(round_tenths_place _)
+bid_results.withColumn(
+  "bid_price_bucket", val round_tenths_place_udf($"bid_price"))
+
+// COMMAND ----------
+
+train.withColumn("test", get_dow(train("pickup_datetime")))
